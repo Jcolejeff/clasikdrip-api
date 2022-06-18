30c606f67a1ad30026d5501cc12980c1e9c03442
jcolejeff <ikwuhj@gmail.com>
2022-06-18 10:01:36 +0100

    updated yarn.lock

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,11 @@ module.exports = ({ env }) => ({
   proxy: true,
   url: env("MY_HEROKU_URL"),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS", [
+      "byIio9e+oBImGPlRmq1Chw==",
+      "SdO0255QoeJvYEVVINV+pw==",
+      "u7E21pzZ+dajAGIGGyI7Gw==",
+      "Qaj4bbjl/JBSZA7f2fA6Ug==",
+    ]),
   },
 });
